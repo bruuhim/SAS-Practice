060c8ce5cb901e8bf0c2b5cdf7c65e3184f0c8e1
bruuhim <ibrahim.xd.hero@gmail.com>
2026-09-18 20:32:16 +0100

    create new function files for Day 3 challenges

--- Days/Day-01/conditions/part-1/challenge11.js
+++ Days/Day-01/conditions/part-1/challenge11.js
@@ -12,6 +12,7 @@ let prix1 = x1 * Quantité;
 let prix2 = x2 * Quantité;
 let prix3 = x3 * Quantité;
 let prix4 = x4 * Quantité;
+let reduction = 0;
 
 switch (Choix) {
   case 1:
@@ -19,11 +20,13 @@ switch (Choix) {
     console.log("Prix unitaire : 60 DH");
     console.log("Sous-total : " + prix1 + " DH");
     if (prix1 >= 200) {
-      console.log("Réduction : " + prix1 * 0.1 + " DH");
+      reduction = prix1 * 0.1;
+      console.log("Réduction : " + reduction + " DH");
     } else if (prix1 < 200) {
+      reduction = 0;
       console.log("Réduction : Aucune réduction");
     }
-    console.log("Total à payer : " + (prix1 - prix1 * 0.1) + " DH");
+    console.log("Total à payer : " + (prix1 - reduction) + " DH");
     break;
 
   case 2:
@@ -31,11 +34,13 @@ switch (Choix) {
     console.log("Prix unitaire : 45 DH");
     console.log("Sous-total : " + prix2 + " DH");
     if (prix2 >= 200) {
-      console.log("Réduction : " + prix2 * 0.1 + " DH");
+      reduction = prix2 * 0.1;
+      console.log("Réduction : " + reduction + " DH");
     } else if (prix2 < 200) {
+      reduction = 0;
       console.log("Réduction : Aucune réduction");
     }
-    console.log("Total à payer : " + (prix2 - prix2 * 0.1) + " DH");
+    console.log("Total à payer : " + (prix2 - reduction) + " DH");
     break;
 
   case 3:
@@ -43,11 +48,13 @@ switch (Choix) {
     console.log("Prix unitaire : 40 DH");
     console.log("Sous-total : " + prix3 + " DH");
     if (prix3 >= 200) {
-      console.log("Réduction : " + prix3 * 0.1 + " DH");
+      reduction = prix3 * 0.1;
+      console.log("Réduction : " + reduction + " DH");
     } else if (prix3 < 200) {
+      reduction = 0;
       console.log("Réduction : Aucune réduction");
     }
-    console.log("Total à payer : " + (prix3 - prix3 * 0.1) + " DH");
+    console.log("Total à payer : " + (prix3 - reduction) + " DH");
     break;
 
   case 4:
@@ -55,11 +62,13 @@ switch (Choix) {
     console.log("Prix unitaire : 30 DH");
     console.log("Sous-total : " + prix4 + " DH");
     if (prix4 >= 200) {
-      console.log("Réduction : " + prix4 * 0.1 + " DH");
+      reduction = prix4 * 0.1;
+      console.log("Réduction : " + reduction + " DH");
     } else if (prix4 < 200) {
+      reduction = 0;
       console.log("Réduction : Aucune réduction");
     }
-    console.log("Total à payer : " + (prix4 - prix4 * 0.1) + " DH");
+    console.log("Total à payer : " + (prix4 - reduction) + " DH");
     break;
 
   default:
--- Days/Day-02/boucles/challenge10.js
+++ Days/Day-02/boucles/challenge10.js
@@ -1,12 +1,14 @@
 let prompt = require("prompt-sync")();
 
 let choix = +prompt("Saisie : ");
+let compteur = 0;
+let somme = 0;
 
-while (choix) {
-  if (choix !== 0) {
-    let choix = +prompt("Saisie : ");
-  } else if (choix == 0) {
-    console.log("Nombre de valeurs : 3");
-  }
+while (choix !== 0) {
+  compteur = compteur + 1;
+  somme = somme + choix;
+  choix = +prompt("Saisie : ");
 }
-console.log("x");
+
+console.log("Nombre de valeurs : " + compteur);
+console.log("Somme : " + somme);

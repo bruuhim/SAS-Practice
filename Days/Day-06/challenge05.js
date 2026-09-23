3754fb5b96da944c7e17b9895a2c6ad49b9899dd
bruuhim <ibrahim.xd.hero@gmail.com>
2026-09-23 21:41:49 +0000

    fixed challenge05.js

--- Days/Day-06/challenge05.js
+++ Days/Day-06/challenge05.js
@@ -8,73 +8,72 @@ let account = {
 let choix;
 menu();
 function menu() {
+  console.log("=========================================================");
   console.log("1. Afficher le solde.");
-  console.log("2. Déposer de l'argent.");
+  console.log("2. Deposer de l'argent.");
   console.log("3. Retirer de l'argent.");
-  console.log("4. Vérifier si le solde est suffisant.");
+  console.log("4. Verifier si le solde est suffisant.");
   console.log("5. Afficher les informations du compte.");
+  console.log("0. Quitter.");
   choix = +prompt("Choisir une option : ");
 }
 
-
-
-if (choix <= 0 || choix > 5) {
-  console.log("Error");
-  menu();
-}
-
-function affichersolde(account) {
-  console.log("Solde actuel : " + account.balance + " DH");
-}
-
-if (choix === 1) {
-  affichersolde(account);
-  menu();
-}
-
-function addsolde(account) {
-  console.log("Solde actuel : " + account.balance + " DH");
-  let add = +prompt("Déposer de l'argent : ");
-   account.balance += add
-   console.log("Solde actuel : " + account.balance + " DH");
-}
-
-if (choix === 2) {
-  addsolde(account);
-  menu();
-}
-
-function rmvsolde(account) {
-  console.log("Solde actuel : " + account.balance + " DH");
-  let rmv = +prompt("Retirer de l'argent : ");
-  console.log("Solde actuel : " + (account.balance - rmv) + " DH");
-}
-
-if (choix === 3) {
-  rmvsolde(account);
-  menu();
-}
-
-function checksolde(account) {
-  if (account.balance > 0) {
-    console.log("Le solde est suffisant.");
-  } else {
-    console.log("Le solde est insuffisant.");
+while (choix !== 0) {
+  if (choix < 0 || choix > 5) {
+    console.log("Error");
   }
-}
 
-if (choix === 4) {
-  checksolde(account);
-  menu();
-}
+  function affichersolde(account) {
+    console.log("Solde actuel : " + account.balance + " DH");
+  }
 
-function affichertout(account) {
-  console.log("Owner: " + account.owner);
-  console.log("Solde actuel : " + account.balance + " DH");
-  console.log("Type: " + account.type);
-}
+  if (choix === 1) {
+    affichersolde(account);
+  }
 
-if (choix === 5) {
-  affichertout(account);
+  function addsolde(account) {
+    console.log("Solde actuel : " + account.balance + " DH");
+    let add = +prompt("Deposer de l'argent : ");
+    account.balance += add;
+    console.log("Solde actuel : " + account.balance + " DH");
+  }
+
+  if (choix === 2) {
+    addsolde(account);
+  }
+
+  function rmvsolde(account) {
+    console.log("Solde actuel : " + account.balance + " DH");
+    let rmv = +prompt("Retirer de l'argent : ");
+    account.balance -= rmv;
+    console.log("Solde actuel : " + account.balance + " DH");
+  }
+
+  if (choix === 3) {
+    rmvsolde(account);
+  }
+
+  function checksolde(account) {
+    if (account.balance > 0) {
+      console.log("Le solde est suffisant.");
+    } else {
+      console.log("Le solde est insuffisant.");
+    }
+  }
+
+  if (choix === 4) {
+    checksolde(account);
+  }
+
+  function affichertout(account) {
+    console.log("Owner: " + account.owner);
+    console.log("Solde actuel : " + account.balance + " DH");
+    console.log("Type: " + account.type);
+  }
+
+  if (choix === 5) {
+    affichertout(account);
+  }
+
   menu();
 }
